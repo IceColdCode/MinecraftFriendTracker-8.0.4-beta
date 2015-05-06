8e16f6513a5f02d11164f75ffea96467f0a28a52
IceColdCode <IceCodeDev@gmail.com>
2015-05-06 16:12:01 -0600

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,1 +1,12 @@
-
+# requires raspberry juice installed in same directory
+import mcpi.Minecraft as minecraft
+import time
+# establish connection
+world = minecraft.Minecraft.create()
+theIds = world.getPlayerIds()
+# main loop
+while True:
+  for id in theIds:
+    name = id + "is online right now!"
+    mc.postToChat(id)
+  time.delay(15)
