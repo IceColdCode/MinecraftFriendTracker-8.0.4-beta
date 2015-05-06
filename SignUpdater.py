c6b4d2d92d36388a6d8673f9718402859689717a
IceColdCode <IceCodeDev@gmail.com>
2015-05-06 16:03:05 -0600

    Update SignUpdater.py

--- SignUpdater.py
+++ SignUpdater.py
@@ -1,10 +1,18 @@
 ###################################################################################################
 # this program is a beta version of a plugin designed for use with bukkit API and raspberry juice #
-# USAGE: class SignUpdater extends Object and can updateText(str) and other functions             #
+# USAGE:  update sign text                                                                        #
 ###################################################################################################
 # imports
 from mcpi import mcpi.Minecraft as minecraft
 from mcpi import mcpi.Block as block
+#initalize minecraft
+world = minecraft.Minecraft.create()
 # SignUpdater class definition
 class SignUpdater(Object):
+  """ this class can deleteSign(x,y,z), createSign(x,y,z), and updateText(x,y,z,str)"""
+  # create sign and set it's text:
+  def createSign(x, y, z, text):
+    string = [text]
+    world.setBlock(x, y, z, block.SIGN.id, string)
+  
   
