5f1647124c62a1b86b1bff9a7f4d0522392976b9
IceColdCode <IceCodeDev@gmail.com>
2015-05-06 15:34:31 -0600

    Create SignUpdater.py

---
+++ SignUpdater.py
@@ -0,0 +1,10 @@
+###################################################################################################
+# this program is a beta version of a plugin designed for use with bukkit API and raspberry juice #
+# USAGE: class SignUpdater extends Object and can updateText(str) and other functions             #
+###################################################################################################
+# imports
+from mcpi import mcpi.Minecraft as minecraft
+from mcpi import mcpi.Block as block
+# SignUpdater class definition
+class SignUpdater(Object):
+  
